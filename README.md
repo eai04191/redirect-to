7696574b7b4b4cb83c82000f6e4cb75f561e92ce
eai04191 <yousei.takahashi@ncdc.co.jp>
2021-08-01 19:03:21 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# vercel-redirect-to
+
+If the return value of the URL specified in the query is a valid URL, api that redirects to that URL.
+
+## Usage
+
+https://vercel-redirect-to.vercel.app/api/?url=https://decapi.me/twitch/avatar/eai_stream
+
+This URL will redirect you to the [twitch.tv/eai_stream](https://www.twitch.tv/eai_stream) icon.
---
+++ api/index.ts
@@ -0,0 +1,24 @@
+import { VercelRequest, VercelResponse } from "@vercel/node";
+import got from "got";
+import validUrl from "valid-url";
+
+export default async (request: VercelRequest, response: VercelResponse) => {
+  const { url = null } = request.query;
+  if (url === null) {
+    response.status(400).json({ error: `Missing url parameter.` });
+    return;
+  }
+  if (typeof url !== "string") {
+    response.status(400).json({ error: `url parameter must be string.` });
+    return;
+  }
+
+  const fetchedResponse = await got(url).text();
+
+  if (!validUrl.isWebUri(fetchedResponse)) {
+    response.status(400).json({ error: `response is not web uri.` });
+    return;
+  }
+
+  response.redirect(fetchedResponse);
+};
